7511b903412450b292fd91df14fa1fcd3be88eb5
haibo <393993705@qq.com>
2016-01-18 22:04:53 +0800

    write run spider

--- run.py
+++ run.py
@@ -1,13 +1,59 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+"""
+run spider in two different ways
+
+it can run in single spider ,selected from BYR,Lagou,Zhilian,51Job or
+run in multiprocessing
+
+"""
+
 
 import time,timeit
+import datetime
 from multiprocessing import Pool,Manager
 from jobspider import BYR_Spider,LG_Spider,ZL_Spider,Job51_Spider
-
+from utils.store_data import Job_Data
 
 lock = Manager().Lock()
+
+
+class Spider():
+
+    spiders = {
+        'byr':BYR_Spider('byr','X-Requested-With','Host','Referer'),
+        'lagou':LG_Spider('lagou'),
+        'zhilian':ZL_Spider('zhilian')
+        '51job':Job51_Spider('51job','Host','Cookie')
+               }
+    def __init__(self,keyword,store_type='mysql'):
+        self.keyword = keyword
+        self.store_type = store_type
+
+    def get_single_data(self,spiname):
+        self.spider = self.spiders[spiname]
+        return self.spider.pages_parse(self.keyword)
+
+    def single_run(self,spiname):
+        db = Job_Data(self.store_type)
+        for data in self.get_single_data(spiname):
+            db.store(data)
+
+    def multi_run(self,spiname):
+        db = Job_Data(self.store_type)
+        for data in self.get_single_data(spiname):
+            lock.acquire()
+            db.store(data)
+            lock.release()
+
+    def producer(self):
+        p = Pool()
+        sites = ['zhilian', '51job','byr','lagou']
+        for site in sites:
+            p.apply_async(self.multi_run,args=(site,))
+        p.close()
+        p.join()
 
 
 def crawl(website,keyword='python'):
@@ -34,13 +80,10 @@ def producer():
 
 
 if __name__=="__main__":
-    #do not add host
-    #lg = LG_Spider('lagou')
-    #print lg.headers
-    #lg.pages_parse('python')
-    #byr = BYR_Spider('byr','X-Requested-With','Host','Referer')
-    #byr.pages_parse('python')
-    #print byr.headers
+    spider = Spider('python')
+    spider.single_run('lagou')
+
+    
 
 
 
--- utils/store_data.py
+++ utils/store_data.py
@@ -20,20 +20,29 @@ class Job_Data():
     def __init__(self,store_type='Mysql'):
         self.store_type = store_type
 
-    def store_to_mysql(self,dbname,table,data):
-        self.db = Database(dbname)
-        self.table = table
-        self.db.insert_dic_by_list(table,data)
+    def store(self,data):
+        if self.store_type == 'MySQL':
+            self.store_to_mysql(data)
+        elif self.store_type == 'json':
+            self.store_to_json(data)
+        elif self.store_type == 'excel':
+            self.store_to_excel(data)
+        elif self.store_type == 'redis':
+            self.store_to_redis(data)
+
+    def store_to_mysql(self,data):
+        self.db = Database('Job')
+        self.db.insert_dic_by_list('jobs',data)
 
     def store_to_json(self,data,filename='job.json'):
         json_data = json.dumps(data)
         with file('job.json','a+') as json_file:
             json_file.write(json_data)
 
-    def write_to_excel(self,data,filename='job.xlsx'):
+    def store_to_excel(self,data,filename='job.xlsx'):
         pass
 
-    def write_to_redis(self,data,keyname='job'):
+    def store_to_redis(self,data,keyname='job'):
         pass
 
     
